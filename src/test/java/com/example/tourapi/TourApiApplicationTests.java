584a8b05bb4793dd9fe08c447ba3ed080fcdbfb1
LAPTOP-BQ9GIK6U\su <tmdduq5498@gmail.com>
2021-10-22 17:09:13 +0900

    initial commit

---
+++ src/test/java/com/example/tourapi/TourApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tourapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
