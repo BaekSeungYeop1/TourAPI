e75c7efc29b837308e048dd497f456c0cee3e494
baekseungyeop <tmdduq5498@gmail.com>
2021-11-04 21:17:14 +0900

    GET /apitest - json형태로 데이터추출(러시아어를 제외하고는 SERVICE_ACCESS_DENIED_ERROR)

--- src/main/java/com/example/tourapi/Controller/RestTestController.java
+++ src/main/java/com/example/tourapi/Controller/RestTestController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -12,33 +11,36 @@ import java.net.URL;
 @RestController
 public class RestTestController {
 
-    @GetMapping(value = "/apitest")
-    public String callApi() throws IOException{
-        StringBuffer result = new StringBuffer();
+    @GetMapping("/apitest")
+    public String callApi() {
+        StringBuilder result = new StringBuilder();
         try {
-            String urlstr = "http://api.visitkorea.or.kr/openapi/service/?" +
-                    "serviceKey= SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D" +
-                    "pageNo=1" +
-                    "numOfRows=10" +
-                    "type= json";
-            URL url = new URL(urlstr);
+            String apiUrl = "http://api.visitkorea.or.kr/openapi/service/rest/RusService/detailInfo?" +
+                    "serviceKey=SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D" +
+                    "&numOfRows=10" +
+                    "&pageNo=1" +
+                    "&MobileOS=ETC" +
+                    "&MobileApp=AppTest" +
+                    "&contentId=336061" +
+                    "&contentTypeId=76" +
+                    "&_type=json";
+
+            URL url = new URL(apiUrl);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
 
-            BufferedReader br;
-            br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
 
             String returnLine;
 
-            while ((returnLine = br.readLine()) != null){
-                result.append(returnLine+"\n\r");
+            while((returnLine = bufferedReader.readLine()) != null) {
+                result.append(returnLine + "\n");
             }
-
             urlConnection.disconnect();
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
         return result.toString();
     }
 }
