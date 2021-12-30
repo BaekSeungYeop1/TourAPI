c1f086b526592903508ff1e15507f48636c945b1
baekseungyeop <tmdduq5498@gmail.com>
2021-12-30 20:55:20 +0900

    /detailinfo - 상세정보페이지로 넘어가기 위한 코드 추가(수정필요)

--- src/main/java/com/example/tourapi/Controller/RestTestController.java
+++ src/main/java/com/example/tourapi/Controller/RestTestController.java
@@ -1,9 +1,7 @@
 package com.example.tourapi.Controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -11,18 +9,21 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 @RestController
+@RequestMapping(value = "/apitest")
 @Slf4j
 public class RestTestController {
 
     // https://wildeveloperetrain.tistory.com/62
-    @GetMapping("/apitest")
-    public String callApi(@RequestParam("tEst")String test) {
+
+    // 지역기반 검색
+    @GetMapping("/")
+    public String getTourApi(@RequestParam String pageNo) {
         StringBuilder result = new StringBuilder();
-        log.info("test = " + test);
+        log.info("pageNo = " + pageNo);
         try {
             String apiUrl = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?" +
                     "serviceKey=SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D" +
-                    "&pageNo=" + test +
+                    "&pageNo=" + pageNo +
                     "&numOfRows=10" +
                     "&MobileApp=AppTest" +
                     "&MobileOS=ETC" +
@@ -50,5 +51,84 @@ public class RestTestController {
 
         return result.toString();
     }
+
+    // 지역 코드 검색
+    @GetMapping("/test")
+    public String ApiTest() {
+        StringBuilder result = new StringBuilder();
+        try {
+            String apiUrl = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?" +
+                    "serviceKey=SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D" +
+                    "&numOfRows=10" +
+                    "&pageNo=1" +
+                    "&MobileOS=ETC" +
+                    "&areaCode=2" +
+                    "&MobileApp=AppTest" +
+                    /*"&contentld=2674675" +
+                    "&contentTypeld=15" +*/
+                    "&_type=json";
+
+            URL url = new URL(apiUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+            String returnLine;
+
+            while((returnLine = bufferedReader.readLine()) != null) {
+                result.append(returnLine + "\n");
+            }
+            urlConnection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+
+    // 상세정보
+    @GetMapping("/detailInfo")
+    public String ApiTest1() {
+        StringBuilder result = new StringBuilder();
+        try {
+            String apiUrl = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro?" +
+                    "serviceKey=SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D" +
+                    "&numOfRows=10" +
+                    "&pageNo=1" +
+                    "&MobileOS=ETC" +
+                    "&MobileApp=AppTest" +
+                    "&contentId=2674675" +
+                    "&contentTypeId=15" +
+                    "&_type=json";
+            // http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro?
+            // serviceKey=SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D
+            // &numOfRows=10
+            // &pageNo=1
+            // &MobileOS=ETC
+            // &MobileApp=AppTest
+            // &contentId=2674675
+            // &contentTypeId=15
+
+            URL url = new URL(apiUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+            String returnLine;
+
+            while((returnLine = bufferedReader.readLine()) != null) {
+                result.append(returnLine + "\n");
+            }
+            urlConnection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+
+
 
 }
