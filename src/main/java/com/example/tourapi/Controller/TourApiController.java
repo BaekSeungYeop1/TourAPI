c6e70a3f0456d5b0f7cf2369382ef4185aa3662a
baekseungyeop <tmdduq5498@gmail.com>
2022-01-03 21:34:51 +0900

    1. Controller,Service 분리 2. GetMapping(/apitest) - (/areacode) 지역정보 조회 - (/subareacode) 시군구코드 정보 조회 - (/searchKeyword) 키워드 조회

---
+++ src/main/java/com/example/tourapi/Controller/TourApiController.java
@@ -0,0 +1,83 @@
+package com.example.tourapi.Controller;
+
+import com.example.tourapi.Service.TourApiService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/apitest")
+@Slf4j
+public class TourApiController {
+
+    public final TourApiService tourApiService;
+    // https://wildeveloperetrain.tistory.com/62
+
+    // 지역기반 검색
+    @GetMapping("/")
+    public String getTourApi(@RequestParam int pageNo) {
+        return tourApiService.getTourApi(pageNo);
+    }
+
+    // 지역코드 정보 조회
+    @GetMapping("/areacode")
+    public String getTourApi() {
+        return tourApiService.getAreaCode();
+    }
+
+    // 지역 코드가(areaCode=?)인 시군구코드 정보 조회
+   @GetMapping("/subareacode")
+    public String getSubAreaCode(@RequestParam int areaCode) {
+        return tourApiService.getSubAreaCode(areaCode);
+    }
+
+    // 키워드 조회
+    @GetMapping("/searchKeyword")
+    public String getKeyword(@RequestParam String Searchkeyword){
+        log.info(Searchkeyword);
+        return tourApiService.getKeyword(Searchkeyword);
+    }
+
+
+    // 상세정보
+    @GetMapping("/detailInfo")
+    public String ApiTest1(@RequestParam int contentId, int contentTypeId) {
+        StringBuilder result = new StringBuilder();
+        try {
+            String apiUrl = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro?" +
+                    "serviceKey=SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D" +
+                    "&numOfRows=10" +
+                    "&pageNo=1" +
+                    "&MobileOS=ETC" +
+                    "&MobileApp=AppTest" +
+                    "&contentId=" + contentId +
+                    "&contentTypeId=" + contentTypeId +
+                    "&_type=json";
+            URL url = new URL(apiUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+            String returnLine;
+
+            while((returnLine = bufferedReader.readLine()) != null) {
+                result.append(returnLine + "\n");
+            }
+            urlConnection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+
+
+
+}
--- src/main/java/com/example/tourapi/Service/TourApiService.java
+++ src/main/java/com/example/tourapi/Service/TourApiService.java
@@ -1,23 +1,21 @@
-package com.example.tourapi.Controller;
+package com.example.tourapi.Service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
-@RestController
-@RequestMapping(value = "/apitest")
+@RequiredArgsConstructor
+@Service
 @Slf4j
-public class RestTestController {
+public class TourApiService {
 
-    // https://wildeveloperetrain.tistory.com/62
-
-    // 지역기반 검색
-    @GetMapping("/")
-    public String getTourApi(@RequestParam String pageNo) {
+    public String getTourApi(int pageNo) {
         StringBuilder result = new StringBuilder();
         log.info("pageNo = " + pageNo);
         try {
@@ -50,22 +48,18 @@ public class RestTestController {
         }
 
         return result.toString();
+
     }
 
-    // 지역 코드 검색
-    @GetMapping("/test")
-    public String ApiTest() {
+    public String getAreaCode() {
         StringBuilder result = new StringBuilder();
         try {
             String apiUrl = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?" +
                     "serviceKey=SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D" +
-                    "&numOfRows=10" +
-                    "&pageNo=1" +
                     "&MobileOS=ETC" +
-                    "&areaCode=2" +
                     "&MobileApp=AppTest" +
-                    /*"&contentld=2674675" +
-                    "&contentTypeld=15" +*/
+                    "&pageNo=1" +
+                    "&numOfRows=20" +
                     "&_type=json";
 
             URL url = new URL(apiUrl);
@@ -87,28 +81,17 @@ public class RestTestController {
         return result.toString();
     }
 
-    // 상세정보
-    @GetMapping("/detailInfo")
-    public String ApiTest1() {
+    public String getSubAreaCode(int areaCode) {
         StringBuilder result = new StringBuilder();
         try {
-            String apiUrl = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro?" +
+            String apiUrl = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?" +
                     "serviceKey=SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D" +
-                    "&numOfRows=10" +
+                    "&numOfRows=50" +
                     "&pageNo=1" +
                     "&MobileOS=ETC" +
+                    "&areaCode=" + areaCode +
                     "&MobileApp=AppTest" +
-                    "&contentId=2674675" +
-                    "&contentTypeId=15" +
                     "&_type=json";
-            // http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro?
-            // serviceKey=SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D
-            // &numOfRows=10
-            // &pageNo=1
-            // &MobileOS=ETC
-            // &MobileApp=AppTest
-            // &contentId=2674675
-            // &contentTypeId=15
 
             URL url = new URL(apiUrl);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -129,6 +112,38 @@ public class RestTestController {
         return result.toString();
     }
 
+    public String getKeyword(String Searchkeyword) {
+        StringBuilder result = new StringBuilder();
+        try {
+            String keyword = URLEncoder.encode(Searchkeyword,"UTF-8");
+            log.info(keyword);
+            String apiUrl = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?" +
+                    "serviceKey=SttmOHJrTnbL%2BYGjAeOeUNscrx%2BJsXNqXqnqgRVVZA8pkTaoOaravKjuPByElQKhnXijl0X6yOEbxKV2rPb6iw%3D%3D" +
+                    "&MobileApp=AppTest" +
+                    "&MobileOS=ETC" +
+                    "&pageNo=1" +
+                    "&numOfRows=10" +
+                    "&listYN=Y" +
+                    "&arrange=A" +
+                    "&keyword=" + keyword +
+                    "&_type=json";
 
+            URL url = new URL(apiUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
 
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+            String returnLine;
+
+            while((returnLine = bufferedReader.readLine()) != null) {
+                result.append(returnLine + "\n");
+            }
+            urlConnection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
 }
