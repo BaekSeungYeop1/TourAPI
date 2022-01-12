19e45cf018537788f9c8b54c60e3dec129b2b3a6
DESKTOP-TPQHNMR\baek <tmdduq5498@gmail.com>
2022-01-12 18:32:03 +0900

    npm run lint 기능을 자동으로 수행하기 위한 설정

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import AreaTourlist from '@/views/AreaTourlist.vue'
+import KeywordTourlist from '@/views/KeywordTourlist.vue'
+import Searchbar from '@/views/Searchbar.vue'
+import Appbar from '@/views/Appbar.vue'
+import CommonInfo from '@/views/CommonInfo.vue'
 
 Vue.use(VueRouter)
 
@@ -14,32 +20,32 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue')
+    component: About
   },
   {
     path: '/areatourlist',
     name: 'AreaTourlist',
-    component: () => import('../views/AreaTourlist.vue')
+    component: AreaTourlist
   },
   {
     path: '/keywordtourlist',
     name: 'KeywordTourlist',
-    component: () => import('../views/KeywordTourlist.vue')
+    component: KeywordTourlist
   },
   {
     path: '/searchbar',
     name: 'Searchbar',
-    component: () => import('../views/Searchbar.vue')
+    component: Searchbar
   },
   {
     path: '/appbar',
     name: 'Appbar',
-    component: () => import('../views/Appbar.vue')
+    component: Appbar
   },
   {
     path: '/commoninfo',
     name: 'CommonInfo',
-    component: () => import('../views/CommonInfo.vue')
+    component: CommonInfo
   }
 ]
 
