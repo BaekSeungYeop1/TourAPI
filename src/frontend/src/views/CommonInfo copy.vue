87bfdf1f6c47d839d487780cbb631ef9d236ee34
DESKTOP-TPQHNMR\baek <tmdduq5498@gmail.com>
2022-01-24 21:53:34 +0900

    상세 정보정보 페이지 수정

--- src/frontend/src/views/CommonInfo copy.vue
+++ src/frontend/src/views/CommonInfo copy.vue
@@ -5,55 +5,22 @@
     <v-card class="pa-3">
       <v-row dense>
         <v-col>
-          <v-card
-            class="pa-3"
-            outline
-          >
-            Col
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card
-            class="pa-3"
-            outline
-          >
-            Col
-          </v-card>
-        </v-col>
-        <v-spacer />
-        <v-col>
-          <v-card
-            class="pa-3"
-            outline
-          >
-            Col
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row no-gutters>
-        <v-spacer />
-        <v-col>
-          <v-card
-            class="pa-3"
-            outline
-          >
-            Col
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card
-            class="pa-3"
-            outline
-          >
-            Col
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card
-            class="pa-3"
-            outline
-          >
-            Col
+          <v-card>
+            <v-carousel>
+              <v-carousel-item
+                v-for="(image,i) in images"
+                :key="i"
+                :src="image.smallimageurl"
+                reverse-transition="fade-transition"
+                transition="fade-transition"
+              />
+            </v-carousel>
+            <v-card-title>
+              {{ tours.title }}
+            </v-card-title>
+            <v-card-text>
+              {{ tours.title }}
+            </v-card-text>
           </v-card>
         </v-col>
       </v-row>
@@ -65,10 +32,11 @@
 export default {
   data: () => ({
     tours: {},
-
+    images: {},
   }),
   mounted(){
     this.getTourCommonInfo();
+    this.getTourDetailImage();
     console.log(this.$refs.map);
     console.log(this.tours.mapx)
     var container = this.$refs.map; //지도를 담을 영역의 DOM 레퍼런스
@@ -85,6 +53,17 @@ var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리
       this.$axios.get("/apitest/commonInfo?" + "contentId=" + contentid)
           .then(response=>{
             this.tours = response.data.response.body.items.item;
+            console.log(response.data);
+          })
+          .catch(e=>{
+            console.log(e);
+          })
+    },
+    getTourDetailImage(){
+      let contentid = this.$route.query.contentid;
+      this.$axios.get("/apitest/detailImage?" + "contentId=" + contentid)
+          .then(response=>{
+            this.images = response.data.response.body.items.item;
             console.log(response.data);
           })
           .catch(e=>{
